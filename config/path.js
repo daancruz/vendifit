acfdd1c11f9a46bf3ecaff2e45375249dfe50f6c
Danilo <daan@MacBook-de-danilo.local>
2025-06-28 23:53:41 -0300

    adiciona ico e meta para compartilhamento

--- config/path.js
+++ config/path.js
@@ -7,11 +7,40 @@ const basePath = isGhPages ? `/${repositoryName}` : '';
 
 window.BASE_URL = location.origin + basePath;
 
-const links = [
+// const metas = [
+//     { property: 'og:title', content: 'Vendifit - Refeições Fitness' },
+//     { property: 'og:description', content: 'Encontre as melhores refeições fitness para sua rotina.' },
+//     { property: 'og:image', content: `${baseUrl}/public/assets/img/icons/logo.png` },
+//     { property: 'og:url', content: window.location.href },
+//     { property: 'og:type', content: 'website' },
+//     { property: 'og:locale', content: 'pt_BR' }
+// ]   
+
+// metas.forEach(data => {
+//     const meta = document.createElement('meta');
+//     meta.setAttribute('property', data.property);
+//     meta.setAttribute('content', data.content);
+//     document.head.appendChild(meta);
+// });
+
+
+const icon = [
+    '/public/assets/img/icons/logo.png',
+];
+
+icon.forEach(path => {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = window.BASE_URL + path;
+    link.type = 'image/x-icon';
+    document.head.appendChild(link);
+});
+
+const css = [
     '/src/assets/styles/css/main.css',
 ];
 
-links.forEach(path => {
+css.forEach(path => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = window.BASE_URL + path;
